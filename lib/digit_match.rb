8249ac6f1e80cd3e628a99efd0ff47b8200edb63
Alice Hsiao <alice.hsiao87@gmail.com>
2018-09-21 19:49:52 -0700

    Tested and implemented digit match. Also commented on time and space complexity

--- lib/digit_match.rb
+++ lib/digit_match.rb
@@ -1,4 +1,21 @@
 # Returns count of digits matching in the two input non-negative integers
+# Time Complexity: O(n) where n represents the number of digits in the number with the least digits
+# Space Complexity: O(1), count/digit_num_one/digit_num_two is auxiliary storage
 def digit_match(number1, number2)
-  raise NotImplementedError
+  return 0 if number1 == nil || number2 == nil
+  count = 0
+
+  while number1 > 0 && number2 > 0 do
+    digit_num_one = number1 % 10
+    digit_num_two = number2 % 10
+
+    if digit_num_one == digit_num_two
+      count += 1
+    end
+
+    number1 /= 10
+    number2 /= 10
+  end
+
+  return count
 end
